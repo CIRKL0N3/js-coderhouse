a0c6bfd50eaf876526940ac8778f7e9a29b28f35
Edgar Hernandez <edgar.lt@hotmail.com>
2022-07-11 20:05:26 -0500

    creacion de repositorio git

---
+++ clase-03-bucles/index.js
@@ -0,0 +1,8 @@
+// Hacer un programa que reciba un dato, a través de un cuadro de diálogo, con la siguiente cadena: "Ingrese par, si desea saber qué números son pares o impar, de caso contrario."
+//Una vez obtenido este dato, a través de un bucle tipo for, establecer un rango numérico, de su preferencia, que sea creciente. 
+// Utilizando una sentencia condicional, evalúe con un if, tres posibles condiciones:
+//1) Si el usuario ingresó "par", que se devuelva en consola todos los números pares entre dicho rango numérico.
+//2) Si el usuario ingresó "impar", que se devuelva en consola todos los números impares entre dicho rango numérico.
+//3) Si el usuario dió cancelar al cuadro de diálogo, que se muestre en consola la siguiente cadena: "No ingresó ninguna instrucción"
+
+
