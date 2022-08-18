421bed9e9e379a442af3d8f0d482a152e53b6257
Edgar Hernandez <edgar.lt@hotmail.com>
2022-08-18 00:09:44 -0500

    funcionalidades avanzadas del proyecto

--- proyecto-final-avance/js/main.js
+++ proyecto-final-avance/js/main.js
@@ -1,55 +1,62 @@
 $(document).ready(function() {
     
-    $('.galeria').bxSlider({
-        mode: 'fade',
-        captions: false,
-        slideWidth: 1200,
-        responsive: true
-    });
+    // Slider
+    if(window.location.href.indexOf('index') > -1) {
+
+        $('.galeria').bxSlider({
+            mode: 'fade',
+            captions: false,
+            slideWidth: 1200,
+            responsive: true,
+            pager: true
+        });
+    }
 
     // Posts
-    let posts = [
-        {
-            title: 'Prueba de titulo 1',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas blanditiis doloribus repellat magni, saepe tempore suscipit a mollitia nisi necessitatibus est, nemo ex libero in dignissimos numquam non ipsum. Vel, eum. Praesentium natus nulla labore eveniet atque asperiores optio mollitia repellat voluptas omnis, delectus ipsam pariatur quas autem iure! Repudiandae velit laudantium quod id libero est dolores commodi minus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio error magnam tempore! Placeat dignissimos perspiciatis, repudiandae quibusdam maxime perferendis nihil delectus doloribus sapiente'
-        },
-        {
-            title: 'Prueba de titulo 2',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas blanditiis doloribus repellat magni, saepe tempore suscipit a mollitia nisi necessitatibus est, nemo ex libero in dignissimos numquam non ipsum. Vel, eum. Praesentium natus nulla labore eveniet atque asperiores optio mollitia repellat voluptas omnis, delectus ipsam pariatur quas autem iure! Repudiandae velit laudantium quod id libero est dolores commodi minus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio error magnam tempore! Placeat dignissimos perspiciatis, repudiandae quibusdam maxime perferendis nihil delectus doloribus sapiente'
-        },
-        {
-            title: 'Prueba de titulo 3',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas blanditiis doloribus repellat magni, saepe tempore suscipit a mollitia nisi necessitatibus est, nemo ex libero in dignissimos numquam non ipsum. Vel, eum. Praesentium natus nulla labore eveniet atque asperiores optio mollitia repellat voluptas omnis, delectus ipsam pariatur quas autem iure! Repudiandae velit laudantium quod id libero est dolores commodi minus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio error magnam tempore! Placeat dignissimos perspiciatis, repudiandae quibusdam maxime perferendis nihil delectus doloribus sapiente'
-        },
-        {
-            title: 'Prueba de titulo 4',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas blanditiis doloribus repellat magni, saepe tempore suscipit a mollitia nisi necessitatibus est, nemo ex libero in dignissimos numquam non ipsum. Vel, eum. Praesentium natus nulla labore eveniet atque asperiores optio mollitia repellat voluptas omnis, delectus ipsam pariatur quas autem iure! Repudiandae velit laudantium quod id libero est dolores commodi minus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio error magnam tempore! Placeat dignissimos perspiciatis, repudiandae quibusdam maxime perferendis nihil delectus doloribus sapiente'
-        },
-        {
-            title: 'Prueba de titulo 5',
-            date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
-            content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas blanditiis doloribus repellat magni, saepe tempore suscipit a mollitia nisi necessitatibus est, nemo ex libero in dignissimos numquam non ipsum. Vel, eum. Praesentium natus nulla labore eveniet atque asperiores optio mollitia repellat voluptas omnis, delectus ipsam pariatur quas autem iure! Repudiandae velit laudantium quod id libero est dolores commodi minus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio error magnam tempore! Placeat dignissimos perspiciatis, repudiandae quibusdam maxime perferendis nihil delectus doloribus sapiente'
-        },
-    ];
+    if(window.location.href.indexOf('index') > -1) {
+        let posts = [
+            {
+                title: 'Prueba de titulo 1',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas blanditiis doloribus repellat magni, saepe tempore suscipit a mollitia nisi necessitatibus est, nemo ex libero in dignissimos numquam non ipsum. Vel, eum. Praesentium natus nulla labore eveniet atque asperiores optio mollitia repellat voluptas omnis, delectus ipsam pariatur quas autem iure! Repudiandae velit laudantium quod id libero est dolores commodi minus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio error magnam tempore! Placeat dignissimos perspiciatis, repudiandae quibusdam maxime perferendis nihil delectus doloribus sapiente'
+            },
+            {
+                title: 'Prueba de titulo 2',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas blanditiis doloribus repellat magni, saepe tempore suscipit a mollitia nisi necessitatibus est, nemo ex libero in dignissimos numquam non ipsum. Vel, eum. Praesentium natus nulla labore eveniet atque asperiores optio mollitia repellat voluptas omnis, delectus ipsam pariatur quas autem iure! Repudiandae velit laudantium quod id libero est dolores commodi minus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio error magnam tempore! Placeat dignissimos perspiciatis, repudiandae quibusdam maxime perferendis nihil delectus doloribus sapiente'
+            },
+            {
+                title: 'Prueba de titulo 3',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas blanditiis doloribus repellat magni, saepe tempore suscipit a mollitia nisi necessitatibus est, nemo ex libero in dignissimos numquam non ipsum. Vel, eum. Praesentium natus nulla labore eveniet atque asperiores optio mollitia repellat voluptas omnis, delectus ipsam pariatur quas autem iure! Repudiandae velit laudantium quod id libero est dolores commodi minus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio error magnam tempore! Placeat dignissimos perspiciatis, repudiandae quibusdam maxime perferendis nihil delectus doloribus sapiente'
+            },
+            {
+                title: 'Prueba de titulo 4',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas blanditiis doloribus repellat magni, saepe tempore suscipit a mollitia nisi necessitatibus est, nemo ex libero in dignissimos numquam non ipsum. Vel, eum. Praesentium natus nulla labore eveniet atque asperiores optio mollitia repellat voluptas omnis, delectus ipsam pariatur quas autem iure! Repudiandae velit laudantium quod id libero est dolores commodi minus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio error magnam tempore! Placeat dignissimos perspiciatis, repudiandae quibusdam maxime perferendis nihil delectus doloribus sapiente'
+            },
+            {
+                title: 'Prueba de titulo 5',
+                date: 'Publicado el dia ' + moment().date() + " de " + moment().format("MMMM") + " del " + moment().format("YYYY"),
+                content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quas blanditiis doloribus repellat magni, saepe tempore suscipit a mollitia nisi necessitatibus est, nemo ex libero in dignissimos numquam non ipsum. Vel, eum. Praesentium natus nulla labore eveniet atque asperiores optio mollitia repellat voluptas omnis, delectus ipsam pariatur quas autem iure! Repudiandae velit laudantium quod id libero est dolores commodi minus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio error magnam tempore! Placeat dignissimos perspiciatis, repudiandae quibusdam maxime perferendis nihil delectus doloribus sapiente'
+            },
+        ];
 
-    posts.forEach((item, index) => {
-        let post = `
-            <article class="post">
-                <h2>${item.title}</h2>
-                <span class="date">${item.date}</span>
-                <p>
-                    ${item.content}
-                </p>
-                <a href="#" class="button-more">Leer más</a>
-            </article>
-        `;
+        posts.forEach((item, index) => {
+            let post = `
+                <article class="post">
+                    <h2>${item.title}</h2>
+                    <span class="date">${item.date}</span>
+                    <p>
+                        ${item.content}
+                    </p>
+                    <a href="#" class="button-more">Leer más</a>
+                </article>
+            `;
 
-        $("#posts").append(post);
-    });
+            $("#posts").append(post);
+        });
+    }
 
     // Selector de tema
     let theme = $("#theme");
@@ -66,6 +73,21 @@ $(document).ready(function() {
         theme.attr("href", "proyecto-final-avance/css/blue.css");
     });
 
+    // Página about
+    let about_theme = $("#about_theme");
+
+    $("#to-green").click(function() {
+        about_theme.attr("href", "css/green.css");
+    });
+
+    $("#to-red").click(function() {
+        about_theme.attr("href", "css/red.css");
+    });
+
+    $("#to-blue").click(function() {
+        about_theme.attr("href", "css/blue.css");
+    });
+
     // Scroll arriba de la web
     $('.subir').click(function(e) {
         e.preventDefault();
@@ -76,5 +98,58 @@ $(document).ready(function() {
 
         return false;
     });
+
+    // Login falso
+
+    $("#login form").submit(function() {
+        let form_name = $("#form_name").val();
+
+        localStorage.setItem("form_name", form_name);
+    });
+
+    let form_name = localStorage.getItem("form_name");
+
+    if(form_name != null && form_name != "undefined") {
+        let about_parrafo = $("#about p");
+        
+        about_parrafo.html("<br><strong>Bienvenido, "+form_name+"</strong> ");
+        about_parrafo.append("<a href='#' id='logout'>Cerrar sesión</a>");
+
+        $("#login").hide();
+
+        $("#logout").click(function() {
+            localStorage.clear();
+            location.reload();
+        });
+    }
+
+    // Acordeon
+
+    if(window.location.href.indexOf('about') > -1) {
+        $("#acordeon").accordion();
+    }
+
+    // Reloj
+    if(window.location.href.indexOf('reloj') > -1) {
+        
+        setInterval(function() {
+            let reloj = moment().format("hh:mm:ss");
+            $('#reloj').html(reloj);
+        }, 1000);
+    }
+
+    // Validación 
+    if(window.location.href.indexOf('contact') > -1) {
+
+        $("form input[name='date']").datepicker( {
+            dateFormat: 'dd-mm-yy'
+        });
+
+        $.validate( {
+            lang: 'es',
+            errorMessagePosition: 'top',
+            scrollToTopOnError: true
+        });
+    }
 
 });
